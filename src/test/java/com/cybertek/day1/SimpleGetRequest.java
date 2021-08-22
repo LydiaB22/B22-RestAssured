de1f8d9010623d65ccc824d9f591edf75a59b0bd
LydiaB22 <svelidiya@gmail.com>
2021-08-22 01:29:47 -0400

    commit 3

---
+++ src/test/java/com/cybertek/day1/SimpleGetRequest.java
@@ -0,0 +1,23 @@
+package com.cybertek.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url = "http://54.205.239.177:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        //send a get request and save response inside the response object
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+    }
+}
